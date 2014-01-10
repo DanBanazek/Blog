d28bddd42f876fe41787289607d73e13f8378f6c
Dan Banazek <dan.banazek@gmail.com>
2014-01-09 21:36:17 -0600

    added email testing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 def create
 @post =Post.find(params[:post_id])
 @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+UserMailer.comments_email.deliver
 redirect_to post_path(@post)
 end
 http_basic_authenticate_with name: "dhh", password: "secret",
